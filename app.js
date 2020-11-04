a7c728595684c8a9380394f3b30b02dd76a0d592
sahil1sk <khannasahil303@gmail.com>
2020-11-04 16:20:21 +0530

    Hitting the API(Login SignUp)

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
@@ -10,8 +9,18 @@ const isAuth = require('./middleware/is-auth'); // for check the auth
 
 const app = express();
 
-app.use(cors()); // using core so that any other url will able to make request
 app.use(bodyParser.json());
+
+// this we use for check the cors request
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use(isAuth); // so for every request this middleware will check that it is authenticated user or not
 
@@ -24,7 +33,7 @@ app.use('/graphql', graphqlHttp({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.gfzth.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true })
 .then(() => {     
-    app.listen(3000, () => {
+    app.listen(4000, () => {
         console.log('Server listen at port 3000');
     });
 })
