80156b75cfccfeed95626d0b5dc3d147e9b8bafb
sahil1sk <khannasahil303@gmail.com>
2020-11-08 08:45:38 +0530

    Finishing Code

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -15,6 +15,12 @@ const userLoader = new DataLoader(userIds => {
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
+    // so here we done sorting by finding it's index in the eventIds array
+    events.sort((a,b) => {
+      return ( 
+        eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+      );
+    });
     return events.map(event => {
       return transformEvent(event);
     });
